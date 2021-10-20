f90720a5079a693866f112cb728446f127111184
Weitix <kirill2000gubin@gmail.com>
2021-10-20 14:33:25 +0300

    fix error

--- api/User.js
+++ api/User.js
@@ -9,7 +9,7 @@ const bcrypt = require("bcrypt");
 
 // Signup
 router.post("/signup", (req, res) => {
-  let {name, email, password, dateOfBirth } = req.body;
+  var {name, email, password, dateOfBirth } = req.body;
   name = name.trim();
   email = email.trim();
   password = password.trim();
