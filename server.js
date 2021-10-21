43903120810cf128405b3720adb49e76016ed60f
Weitix <kirill2000gubin@gmail.com>
2021-10-21 15:37:48 +0300

    ыв

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ require("./config/db");
 const app = require("express")();
 const port = process.env.PORT || 3000;
 
-//cors
-const cors = require("cors");
-app.use(cors());
+
+
 
 const UserRouter = require("./api/User");
 
 // For accepting post form data
 const bodyParser = require("express").json;
 app.use(bodyParser());
+
+//cors
+const cors = require("cors");
+app.use(cors());
 
 app.use("/user", UserRouter);
 
