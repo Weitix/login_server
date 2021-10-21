68db69aa9641fb0adf45aa53ea4debad0a725afb
Weitix <kirill2000gubin@gmail.com>
2021-10-21 11:13:31 +0300

    hz

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 
 mongoose
-    .connect(process.env.MONGOBD_URI, {
-        useNewUrlParser: true, 
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        console.log("DB Connected");
-    }) 
-.catch((err) => console.log(err));
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => console.log(err));
